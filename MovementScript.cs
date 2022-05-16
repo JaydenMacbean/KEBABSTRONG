633cf3d644f103e9604cb6d02971a4eb06c65299
Jayden Macbean <61696738+JaydenMacbean@users.noreply.github.com>
2022-05-16 12:14:12 +0200

    I suck at coding :D
    Sup Monkey

--- MovementScript.cs
+++ MovementScript.cs
@@ -1,3 +1,27 @@
 class MovementScript{
+  
+  Vector3 DefaultScale;
+  DefaultScale = transform.localScale;
+  
+  void Update(){
+      
+        //continue crouching if the player is already crouching and holding the left control button
+        //timer will affect this if statement if the player is not crouching and clicks the left control button
+        if(Input.GetKey(") && (timer <= 0 || isCrouching)){ 
+          transform.localScale = new Vector3(10, 10, 10);
+          transform.position = new Vector3(0, 5, 0);
+          //if I understood correctly, the two lines above were responsible for making a game object crouch
+          timer = 3f; 
+          isCrouching = true;
+        }
+        else
+        {
+          isCrouching = false;
+          timer -= Time.deltaTime;
+          //whatever code you had there that would make a game object stop crouching
+          transform.localScale = DefaultScale;
+        }    
+  }
+  
 
 }
